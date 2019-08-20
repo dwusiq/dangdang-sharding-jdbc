1467acb36b658f4e24a9290a56692eca0a1785ef
wusiq <dwusiq@163.c0m>
2019-08-20 15:52:06 +0800

    sharding sphere分表实例,添加数据库脚本

---
+++ sharding-table/script/mydb.sql
@@ -0,0 +1,40 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50723
+ Source Host           : localhost:3306
+ Source Schema         : mydb
+
+ Target Server Type    : MySQL
+ Target Server Version : 50723
+ File Encoding         : 65001
+
+ Date: 20/08/2019 15:50:21
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_user_0
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user_0`;
+CREATE TABLE `tb_user_0`  (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tb_user_1
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user_1`;
+CREATE TABLE `tb_user_1`  (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
